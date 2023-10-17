e8ba8ad67ff96bbcbb98282feb9854c49362d5f9
jeffcaradona <jeffcaradona@gmail.com>
2023-10-17 08:08:50 -0500

    .

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const paramList = ["param1", "param2", "param3"];
 
+const paramList = ["param1", "param2", "param3"];
 paramList.forEach((current, index) => {
+  console.log(`\tCreating ${current}`);
   app.param(current, function (req, res, next, value) {    
-    console.info(`${current}:`, value);
+    console.info(`\t:${current}:`, value);
     next();
   });
 });
 
-app.get("/:param1", (req, res, next) => {
-  console.dir(app.mountpath + ":param1 " + req.params.param1);
-  next();
+app.get("/:param1", (req, res) => {
+  console.dir(":param1 " + req.params.param1);
+    res.send(`${req.params.param1}`);
 });
 
 app.get("/", (req, res) => {
-  console.dir(app.mountpath);
+
   res.send("Hello World!");
+
 });
 
 app.listen(port, () => {
